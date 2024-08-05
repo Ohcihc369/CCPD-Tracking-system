8abc21823dee095d7f76e3b21ff557e8315c04f6
washingotn sanga <sibusisosanga@gmail.com>
2024-08-05 11:04:08 +0200

    new commit

--- app.py
+++ app.py
@@ -1,8 +1,91 @@
-
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+bcrypt = Bcrypt(app)
 
-if __name__ == "__main__":
+# Define the User model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+# Define the Project model
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    votes = db.Column(db.Integer, default=0)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+@login_required
+def submit_project():
+    if request.method == 'POST':
+        project_name = request.form['project_name']
+        new_project = Project(name=project_name)
+        db.session.add(new_project)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('submit.html')
+
+@app.route('/vote', methods=['GET', 'POST'])
+@login_required
+def vote():
+    projects = Project.query.all()
+    if request.method == 'POST':
+        project_id = request.form['project_id']
+        project = Project.query.get(project_id)
+        project.votes += 1
+        db.session.commit()
+        return redirect(url_for('vote'))
+    return render_template('vote.html', projects=projects)
+
+if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
-
